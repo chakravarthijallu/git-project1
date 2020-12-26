2db52997cb7d692a585ddfce56d950f05ad52111
unknown <chakrijallu7@gmail.com>
2020-12-26 20:30:07 +0530

    java_variables_completed

---
+++ CORE JAVA/Test.java
@@ -0,0 +1,21 @@
+class Test 
+{
+public static void main(String[] args)
+{
+	/*
+'''
+double d = 1.2e3;
+int x = 23;
+byte b = 10;
+short s = 34;
+long l = 456;
+System.out.println("Hello");
+System.out.println(d);
+System.out.println(x+"..."+b+"..."+s+"..."+l);
+''' */
+char ch = '\u0061';
+double d = _1_2_3___456.7_8_9;
+System.out.println(ch);
+}
+}
+
---
+++ CORE JAVA/java_trailer.java
@@ -0,0 +1,76 @@
+// importance of java:-
+// class elements:-
+
+class Test2 
+{   
+	/*
+	1) variables
+    2) methods
+	3) constructors
+	4) instance blocks
+	5) static blocks
+*/
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
+
+/*
+
+keywords of java: (50)
+----------------------
+data types:- byte ,short ,int ,float ,double ,char ,boolean  -->8
+flow control :- if, else , switch, case , default , break, for,while,do,continue -->10
+Exception handling :- try,catch,finally,throw,throws -->5
+Object level:- new,this,super,instanceof -->4
+methods :- void , return -->2
+java5 :- enum,assert -->2
+un used keywords :- goto, const -->2
+source file :- class , extends , interface, implements,package,import -->6
+modifiers:- public,private,protected,final,static,abstract,synchronized,strictfp,volatile,
+            transient,native --> 11
+
+java methods :-
+---------------
+1) instance method
+2) static method
+3) normal methods
+4) abstract methods
+6) accessor methods (getters)
+7) mutator methods  (setters)
+8) overidden method
+9) overriding method
+10) final method
+11) native methods
+12) strictfp methods
+13) public methods
+14) private methods
+15) protected methods
+16) default method
+17) instance factory method
+18) static factory method
+19) pattern method
+20) synchronized method
+21) non-synchronized method
+22) in-line methods
+23) call-back methods.
+
+java classes :-
+---------------
+1) normal classes
+2) abstract classes
+3) final classes
+4) strictfp class
+5) immutable class
+6) mutable class
+7) public class
+8) default class
+9) single tan class
+
+*/
+
+
+
+
+
